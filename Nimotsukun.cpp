f4e5f6384de6e34e5ac57b715f30166e6d0cd94c
shoji-k <koba.s0125@gmail.com>
2013-01-03 21:44:32 +0900

    input character

--- Nimotsukun.cpp
+++ Nimotsukun.cpp
@@ -1,11 +1,17 @@
 // Nimotsukun.cpp : メイン プロジェクト ファイルです。
 
 #include "stdafx.h"
+#include<iostream>
 
 using namespace System;
+using namespace std;
 
 int main(array<System::String ^> ^args)
 {
+	char c;
+	cin >> c;
+	cout << "Input character is " << c;
+
     Console::WriteLine(L"Hello World");
     return 0;
 }
